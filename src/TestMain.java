b38bf1846cccf86f42ac10e9a99d1008c421d5d1
Anshika-tech <srivastavanshika10@gmail.com>
2021-09-18 20:51:04 +0530

    basic of java

---
+++ src/TestMain.java
@@ -0,0 +1,22 @@
+/* class - class are user defined data types that can hold both data and method.
+A class is a collection of objects that have identical properties, common behavior and shared relationship.
+object- An object is a collection of data members and associated member functions.
+ */
+
+class Test{
+    int a; //instance variable
+    String name; //instance variable
+}
+public class TestMain {
+    public static void main(String[] args) {
+        Test obj1 =new Test(); //creating object of class Test
+        obj1.a= 10;  //initialisation of object.
+        obj1.name= "Anshika";
+        Test obj2=new Test();
+        obj2.a=20;
+        obj2.name="WELCOME to JAVA TUTORIALS";
+        System.out.println(obj1.a+" "+obj1.name);
+        System.out.println(obj2.a+" "+obj2.name);
+    }
+
+}
