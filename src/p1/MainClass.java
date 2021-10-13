94ef0e3c43c34a06cbcc8ea1c0d97f50243cfbbd
Anshika-tech <srivastavanshika10@gmail.com>
2021-10-14 00:58:44 +0530

    packages in java

---
+++ src/p1/MainClass.java
@@ -0,0 +1,10 @@
+//make a package in which two public class exists one is main class and other is of name Teacher class.
+package p1;
+import p1.Teachers;
+public class MainClass {
+    public static void main(String[] args) {
+        Teachers obj=new Teachers();
+        obj.display();
+
+    }
+}
---
+++ src/p1/Teachers.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class Teachers {
+    int roll;
+    String name;
+     Teachers(){
+         roll=10;
+         name="Anshika";
+     }
+    public void display(){
+        System.out.println(roll);
+        System.out.println(name);
+    }
+}
