5f4430dc00ddf69caa4e3a0b806a49b11368451f
Anshika-tech <srivastavanshika10@gmail.com>
2021-10-15 16:22:03 +0530

    Interface concept

--- src/Interface.java
+++ src/Interface.java
@@ -1,6 +1,6 @@
 /* interface is blueprint of class .There can be only abstract method in interface not method body.
 used to achieve abstraction and multiple inheritance in java.
- */
+
 interface Printable{
     void print();
 }
@@ -11,5 +11,27 @@ class Interface implements Printable{
     public static void main(String[] args) {
         Interface obj=new Interface();
         obj.print();
+    }
+}*/
+//q drawable example.
+interface Shape{
+    void draw();
+}
+class Rectangle implements Shape{
+   public void draw(){
+        System.out.println("drawing rectangle");
+    }
+}
+class Circle implements Shape{
+    public void draw(){
+        System.out.println("drawing circle");
+    }
+}
+public class Interface{
+    public static void main(String[] args) {
+        Shape obj=new Rectangle();
+        Shape obj1=new Circle();
+        obj.draw();
+        obj1.draw();
     }
 }
