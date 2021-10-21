111a046ab8e6db62b772a1f9b14fcbfd79dc2dfc
Anshika-tech <srivastavanshika10@gmail.com>
2021-10-21 23:46:03 +0530

    Anonymous concept

--- src/Anonymous.java
+++ src/Anonymous.java
@@ -2,7 +2,7 @@
 it can be created in two ways:
 1.class(abstract or concrete)
 2.interface
- */
+
 abstract class Person{
     abstract void eat();
 }
@@ -10,6 +10,20 @@ public class Anonymous {
     public static void main(String[] args) {
         Person p = new Person() {
             void eat() {
+                System.out.println("nice fruits.");
+            }
+        };
+        p.eat();
+    }
+}*/
+//by interface method.
+interface Person{
+     void eat();
+}
+public class Anonymous {
+    public static void main(String[] args) {
+        Person p =new Person() {
+            public void eat() {
                 System.out.println("nice fruits.");
             }
         };
