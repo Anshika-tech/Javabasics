bdc905e0d6e9b4a3ed5c92b513edddb9d7d9d86e
Anshika-tech <srivastavanshika10@gmail.com>
2021-11-09 22:32:25 +0530

    wrapper class

---
+++ src/Wrapper.java
@@ -0,0 +1,10 @@
+//Autoboxing -conversion of primitive datatype into its corresponding wrapper class
+public class Wrapper {
+    public static void main(String[] args) {
+    int a=20;
+    Integer i=Integer.valueOf(a);//change int to Integer explicitly
+    Integer j=a;//autoboxing, compiler will write Integer.valueOf(a) internally
+        System.out.println(a+" "+i+" "+j);
+
+    }
+}
