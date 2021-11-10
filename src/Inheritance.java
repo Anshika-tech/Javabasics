e851de634cf3637311c15b02b1e56165997c4067
Anshika-tech <srivastavanshika10@gmail.com>
2021-11-11 00:13:59 +0530

    inheritance

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -18,8 +18,8 @@ public class Inheritance{
         obj.bark();
         obj.eat();
     }
-}*/
-//q1.print salary and bonus of an employee by use of  inheritance.
+}
+//q1.print salary and bonus of an employee by use of inheritance.
 class Employee{
     float salary=40000;
 }
@@ -29,5 +29,40 @@ class programmer extends Employee{
         programmer obj=new programmer();
         System.out.println(obj.salary);
         System.out.println(obj.bonus);
+    }
+}*/
+//create one parent class and subclass and then create two classes, one inherit parent class and other inherit
+//subclass.
+import java.util.Scanner;
+class Person{
+    void m1(){
+        System.out.println("enter salary and subject of teacher");
+    }
+}
+class Student extends Person{
+    void m2(){
+        System.out.println("enter year and course of college student");
+    }
+}
+class Teacher extends Person{
+    void run(int a,String sub){
+        System.out.println(a+" "+sub);
+    }
+}
+class collegestu extends Student{
+    void walk(int year,String course){
+        System.out.println(year+" "+course);
+    }
+}
+public class Inheritance{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        collegestu obj=new collegestu();
+        obj.m2();
+        obj.walk(sc.nextInt(),sc.next());
+        sc.next();
+        Teacher obj1=new Teacher();
+        obj1.m1();
+        obj1.run(sc.nextInt(),sc.next());
     }
 }
