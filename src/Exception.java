0a1406a2498467c24e5aac3f3ade7a0d93605df8
Anshika-tech <srivastavanshika10@gmail.com>
2021-11-13 11:44:17 +0530

    Exception handling

--- src/Exception.java
+++ src/Exception.java
@@ -6,7 +6,7 @@ public class Exception {
            int a=10;
         System.out.println(a/0);//generate exception named arithmetic exception.
     }
-}*/
+}
 //using exception handling-
 public class Exception {
     public static void main(String[] args) {
@@ -17,5 +17,25 @@ public class Exception {
             System.out.println(obj);
         }
         System.out.println("hello");
+    }
+}*/
+//Multiple catch block to handle multiple exception
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Exception{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        try{
+            a=sc.nextInt();//input mismatch exception may occur.
+            b=sc.nextInt();//input mismatch exception may occur.
+            System.out.println(a/b);//arithmetic exception may occur.
+        }
+        catch(ArithmeticException obj){
+            System.out.println("cannot divide by zero");
+        }
+        catch(InputMismatchException e){
+            System.out.println("only numbers are accepted");
+        }
     }
 }
