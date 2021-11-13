a8cbb24b7f781b7f8c1c06ec3496039782d5996f
Anshika-tech <srivastavanshika10@gmail.com>
2021-11-13 12:26:32 +0530

    Exception handling

--- src/Exception.java
+++ src/Exception.java
@@ -18,7 +18,7 @@ public class Exception {
         }
         System.out.println("hello");
     }
-}*/
+}
 //Multiple catch block to handle multiple exception
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -36,6 +36,19 @@ public class Exception{
         }
         catch(InputMismatchException e){
             System.out.println("only numbers are accepted");
+        }
+    }
+}*/
+import java.util.Scanner;
+public class Exception{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        try {
+            int a = Integer.parseInt(sc.next());//if input is in the form of string like "anshika"
+            // then NumberFormatException occur.
+            System.out.println("square of number is "+a*a);
+        }catch(NumberFormatException e){
+            System.out.println("Enter input is not valid format for an integer.");
         }
     }
 }
