ecdb91e5705c8d51d9dcd2903a3ab23c6e01c7ad
Anshika-tech <srivastavanshika10@gmail.com>
2021-11-23 00:25:36 +0530

    Multithreading concept

--- src/Multithreading.java
+++ src/Multithreading.java
@@ -1,5 +1,5 @@
 //multithreading- Multithreading in Java is a process of executing multiple threads simultaneously.
-public class Multithreading {
+/*public class Multithreading {
         public static void main(String[] args) {
             Thread obj=Thread.currentThread();//It returns a reference to the currently executing thread object.
             System.out.println(obj.getName());//It returns the name of the thread.
@@ -10,5 +10,16 @@ public class Multithreading {
             System.out.println(obj.getPriority());//It returns the priority of the thread.
             System.out.println(obj.isDaemon());//It tests if the thread is a daemon thread.
         }
-
+}
+*/
+// two ways to create Thread
+//1. by extending Thread class
+class Test extends Thread{
+    public void run(){
+        System.out.println("Thread is running");
+    }
+    public static void main(String[] args) {
+      Test T1=new Test();
+      T1.start();
+    }
 }
