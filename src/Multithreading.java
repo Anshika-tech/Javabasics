82628b8d5fec2812d76150ba4edad9d3d4236477
Anshika-tech <srivastavanshika10@gmail.com>
2021-11-23 16:39:35 +0530

    Multithreading concept

--- src/Multithreading.java
+++ src/Multithreading.java
@@ -22,7 +22,7 @@ class Test extends Thread{
       Test T1=new Test();
       T1.start();
     }
-}*/
+}
 //2.by implementing Runnable interface
 class Test implements Runnable{
     public void run(){
@@ -31,6 +31,31 @@ class Test implements Runnable{
     public static void main(String[] args) {
         Test m1 =new Test();
         Thread T1=new Thread(m1);
+        T1.start();
+    }
+}*/
+//q.create thread which print 1 to 10 after printing 5 ,there should be delay of 5000 before
+// printing 6.
+class Test extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            try {
+                if (i != 6)
+                    System.out.println(i);
+                else
+                {
+                    Thread.sleep(5000);
+                    System.out.println(i);
+                }
+            }catch(Exception e){
+                System.out.println("exception is :"+e);
+            }
+        }
+    }
+}
+public class Multithreading{
+    public static void main(String[] args) {
+        Test T1=new Test();
         T1.start();
     }
 }
